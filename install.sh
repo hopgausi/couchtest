65a80bba86b42a0e83815686bbc4f204f97fed7e
hopgausi <hopgausi20@gmail.com>
2022-10-20 12:48:11 +0200

    istall

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@
 sudo systemctl stop couchdb
 sudo apt-get remove couchdb
 sudo apt-get purge couchdb
+sudo dpkg -i libnspr4_4.11-1ubuntu1_amd64.deb
 sudo dpkg -i couch-libmozjs185-1.0_1.8.5-1.0.0+couch-2_xenial_amd64.deb
 sudo dpkg -i couchdb_3.2.1_xenial_amd64.deb
